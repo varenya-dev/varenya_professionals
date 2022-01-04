c32be6a92be78d77abadae8905139eaa2c2798c5
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 09:01:26 +0530

    Service Implementation for updating posts. Closes #82

--- lib/services/post.service.dart
+++ lib/services/post.service.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:varenya_professionals/constants/endpoint_constant.dart';
 import 'package:varenya_professionals/dtos/post/create_post/create_post.dto.dart';
+import 'package:varenya_professionals/dtos/post/update_post/update_post.dto.dart';
 import 'package:varenya_professionals/exceptions/server.exception.dart';
 import 'package:varenya_professionals/models/post/post.model.dart';
 import 'package:varenya_professionals/models/post/post_category/post_category.model.dart';
@@ -139,6 +140,36 @@ class PostService {
     http.Response response = await http.post(
       uri,
       body: createPostDto.toJson(),
+      headers: headers,
+    );
+
+    // Check for any errors.
+    if (response.statusCode >= 400 && response.statusCode < 500) {
+      Map<String, dynamic> body = json.decode(response.body);
+      throw ServerException(message: body['message']);
+    } else if (response.statusCode >= 500) {
+      throw ServerException(
+          message: 'Something went wrong, please try again later.');
+    }
+  }
+
+  Future<void> updatePost(UpdatePostDto updatePostDto) async {
+    // Fetch the ID token for the user.
+    String firebaseAuthToken =
+    await this._firebaseAuth.currentUser!.getIdToken();
+
+    // Prepare URI for the request.
+    Uri uri = Uri.parse("$ENDPOINT/post");
+
+    // Prepare authorization headers.
+    Map<String, String> headers = {
+      "Authorization": "Bearer $firebaseAuthToken",
+    };
+
+    // Send the post request to the server.
+    http.Response response = await http.put(
+      uri,
+      body: updatePostDto.toJson(),
       headers: headers,
     );
 

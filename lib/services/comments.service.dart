48356a7438d20ddaf0e4a0ffdc974f517c0a8b1b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 14:56:04 +0530

    Service Implementation for updating comments. Closes #102

--- lib/services/comments.service.dart
+++ lib/services/comments.service.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:varenya_professionals/constants/endpoint_constant.dart';
 import 'package:varenya_professionals/dtos/comments/create_comment/create_comment.dto.dart';
+import 'package:varenya_professionals/dtos/comments/update_comment/update_comment.dto.dart';
 import 'package:varenya_professionals/exceptions/server.exception.dart';
 
 
@@ -27,6 +28,37 @@ class CommentsService {
     http.Response response = await http.post(
       uri,
       body: createCommentDto.toJson(),
+      headers: headers,
+    );
+
+    // Check for any errors.
+    if (response.statusCode >= 400 && response.statusCode < 500) {
+      Map<String, dynamic> body = json.decode(response.body);
+      print(response.body);
+      throw ServerException(message: body['message']);
+    } else if (response.statusCode >= 500) {
+      throw ServerException(
+          message: 'Something went wrong, please try again later.');
+    }
+  }
+
+  Future<void> updateComment(UpdateCommentDto updateCommentDto) async {
+    // Fetch the ID token for the user.
+    String firebaseAuthToken =
+    await this._firebaseAuth.currentUser!.getIdToken();
+
+    // Prepare URI for the request.
+    Uri uri = Uri.parse("$ENDPOINT/comment");
+
+    // Prepare authorization headers.
+    Map<String, String> headers = {
+      "Authorization": "Bearer $firebaseAuthToken",
+    };
+
+    // Send the post request to the server.
+    http.Response response = await http.put(
+      uri,
+      body: updateCommentDto.toJson(),
       headers: headers,
     );
 

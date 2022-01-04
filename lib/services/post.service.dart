d66e463a21d7147be9f83d5c7f2e0634eb9ffc25
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 13:34:14 +0530

    Update for Post Services

--- lib/services/post.service.dart
+++ lib/services/post.service.dart
@@ -49,9 +49,13 @@ class PostService {
   }
 
   Future<List<Post>> fetchPostsByCategory(String category) async {
+    if (category == 'NEW') {
+      return await this.fetchNewPosts();
+    }
+
     // Fetch the ID token for the user.
     String firebaseAuthToken =
-    await this._firebaseAuth.currentUser!.getIdToken();
+        await this._firebaseAuth.currentUser!.getIdToken();
 
     // Prepare URI for the request.
     Uri uri = Uri.http(
@@ -83,7 +87,7 @@ class PostService {
 
     List<dynamic> jsonResponse = json.decode(response.body);
     List<Post> posts =
-    jsonResponse.map((postJson) => Post.fromJson(postJson)).toList();
+        jsonResponse.map((postJson) => Post.fromJson(postJson)).toList();
 
     return posts;
   }
@@ -91,7 +95,7 @@ class PostService {
   Future<List<PostCategory>> fetchCategories() async {
     // Fetch the ID token for the user.
     String firebaseAuthToken =
-    await this._firebaseAuth.currentUser!.getIdToken();
+        await this._firebaseAuth.currentUser!.getIdToken();
 
     // Prepare URI for the request.
     Uri uri = Uri.parse("$ENDPOINT/post/categories");
@@ -119,7 +123,7 @@ class PostService {
 
     List<dynamic> jsonResponse = json.decode(response.body);
     List<PostCategory> categories =
-    jsonResponse.map((json) => PostCategory.fromJson(json)).toList();
+        jsonResponse.map((json) => PostCategory.fromJson(json)).toList();
 
     return categories;
   }
@@ -127,7 +131,7 @@ class PostService {
   Future<void> createNewPost(CreatePostDto createPostDto) async {
     // Fetch the ID token for the user.
     String firebaseAuthToken =
-    await this._firebaseAuth.currentUser!.getIdToken();
+        await this._firebaseAuth.currentUser!.getIdToken();
 
     // Prepare URI for the request.
     Uri uri = Uri.parse("$ENDPOINT/post");
@@ -135,18 +139,20 @@ class PostService {
     // Prepare authorization headers.
     Map<String, String> headers = {
       "Authorization": "Bearer $firebaseAuthToken",
+      'Content-type': 'application/json',
     };
 
     // Send the post request to the server.
     http.Response response = await http.post(
       uri,
-      body: createPostDto.toJson(),
+      body: json.encode(createPostDto.toJson()),
       headers: headers,
     );
 
     // Check for any errors.
     if (response.statusCode >= 400 && response.statusCode < 500) {
       Map<String, dynamic> body = json.decode(response.body);
+      print(response.body);
       throw ServerException(message: body['message']);
     } else if (response.statusCode >= 500) {
       throw ServerException(
@@ -157,7 +163,7 @@ class PostService {
   Future<void> updatePost(UpdatePostDto updatePostDto) async {
     // Fetch the ID token for the user.
     String firebaseAuthToken =
-    await this._firebaseAuth.currentUser!.getIdToken();
+        await this._firebaseAuth.currentUser!.getIdToken();
 
     // Prepare URI for the request.
     Uri uri = Uri.parse("$ENDPOINT/post");
@@ -165,12 +171,13 @@ class PostService {
     // Prepare authorization headers.
     Map<String, String> headers = {
       "Authorization": "Bearer $firebaseAuthToken",
+      'Content-type': 'application/json',
     };
 
     // Send the post request to the server.
     http.Response response = await http.put(
       uri,
-      body: updatePostDto.toJson(),
+      body: json.encode(updatePostDto.toJson()),
       headers: headers,
     );
 
@@ -187,7 +194,7 @@ class PostService {
   Future<void> deletePost(DeletePostDto deletePostDto) async {
     // Fetch the ID token for the user.
     String firebaseAuthToken =
-    await this._firebaseAuth.currentUser!.getIdToken();
+        await this._firebaseAuth.currentUser!.getIdToken();
 
     // Prepare URI for the request.
     Uri uri = Uri.parse("$ENDPOINT/post");

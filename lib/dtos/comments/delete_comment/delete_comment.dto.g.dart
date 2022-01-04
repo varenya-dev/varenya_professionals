9899f7aec12d40916431cebefeef6dfcc71e49d1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 14:53:49 +0530

    DTO Implementation for deleting comments. Closes #100

---
+++ lib/dtos/comments/delete_comment/delete_comment.dto.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'delete_comment.dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DeleteCommentDto _$DeleteCommentDtoFromJson(Map<String, dynamic> json) =>
+    DeleteCommentDto(
+      commentId: json['commentId'] as String,
+    );
+
+Map<String, dynamic> _$DeleteCommentDtoToJson(DeleteCommentDto instance) =>
+    <String, dynamic>{
+      'commentId': instance.commentId,
+    };

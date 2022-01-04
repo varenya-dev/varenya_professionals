87c31327e343c6eb29edb5f1f22dbccbacc77510
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 08:47:53 +0530

    Updated server user model with random name and doctor details. Closes #77

--- lib/models/server_user/server_user.model.dart
+++ lib/models/server_user/server_user.model.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:varenya_professionals/enum/roles.enum.dart';
+import 'package:varenya_professionals/models/doctor/doctor.model.dart';
+import 'package:varenya_professionals/models/server_user/random_name/random_name.model.dart';
 
 part 'server_user.model.g.dart';
 
@@ -11,10 +13,18 @@ class ServerUser {
   String firebaseId;
   Roles role;
 
+  @JsonKey(defaultValue: null)
+  Doctor? doctor;
+
+  @JsonKey(defaultValue: null)
+  RandomName? randomName;
+
   ServerUser({
     required this.id,
     required this.firebaseId,
     required this.role,
+    required this.doctor,
+    required this.randomName,
   });
 
   factory ServerUser.fromJson(Map<String, dynamic> json) =>
--- lib/models/server_user/server_user.model.g.dart
+++ lib/models/server_user/server_user.model.g.dart
@@ -10,6 +10,12 @@ ServerUser _$ServerUserFromJson(Map<String, dynamic> json) => ServerUser(
       id: json['id'] as String,
       firebaseId: json['firebaseId'] as String? ?? '',
       role: $enumDecode(_$RolesEnumMap, json['role']),
+      doctor: json['doctor'] == null
+          ? null
+          : Doctor.fromJson(json['doctor'] as Map<String, dynamic>),
+      randomName: json['randomName'] == null
+          ? null
+          : RandomName.fromJson(json['randomName'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$ServerUserToJson(ServerUser instance) =>
@@ -17,6 +23,8 @@ Map<String, dynamic> _$ServerUserToJson(ServerUser instance) =>
       'id': instance.id,
       'firebaseId': instance.firebaseId,
       'role': _$RolesEnumMap[instance.role],
+      'doctor': instance.doctor,
+      'randomName': instance.randomName,
     };
 
 const _$RolesEnumMap = {

52487b3302c1d9a048a78dad9321ad8ece30c2f6
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 08:54:04 +0530

    Model Implementation for post. Closes #78

---
+++ lib/enum/post_type.enum.dart
@@ -0,0 +1,4 @@
+enum PostType {
+  Post,
+  Comment,
+}
---
+++ lib/models/post/post_category/post_category.model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_category.model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PostCategory _$PostCategoryFromJson(Map<String, dynamic> json) => PostCategory(
+      id: json['id'] as String,
+      categoryName: json['categoryName'] as String,
+    );
+
+Map<String, dynamic> _$PostCategoryToJson(PostCategory instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'categoryName': instance.categoryName,
+    };

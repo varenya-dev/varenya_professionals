ad5fd223372beeb87f218b1154b30bdf42d3ec91
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-27 14:54:46 +0530

    Dynamic base endpoint for web and mobile

--- lib/constants/endpoint_constant.dart
+++ lib/constants/endpoint_constant.dart
@@ -1,2 +1,4 @@
-const ENDPOINT = "http://10.0.2.2:5000/v1/api";
-const RAW_ENDPOINT = "10.0.2.2:5000";
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+const ENDPOINT = kIsWeb ? "http://127.0.0.1:5000/v1/api" : "http://10.0.2.2:5000/v1/api";
+const RAW_ENDPOINT = kIsWeb ? "127.0.0.1:5000" : "10.0.2.2:5000";

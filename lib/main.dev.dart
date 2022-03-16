d653c6fd03f36c9e1d02f07e98d5b2ae347cf735
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-16 08:59:04 +0530

    Dev vs Prod Config

---
+++ lib/main.dev.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:varenya_professionals/app.dart';
+import 'package:varenya_professionals/providers.dart';
+import 'package:varenya_professionals/setup.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await fullSetup();
+
+  runApp(DevRoot());
+}
+
+class DevRoot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Providers(
+      apiUrl: kIsWeb ? "http://127.0.0.1:5000/v1/api" : "http://10.0.2.2:5000/v1/api",
+      rawApiUrl: kIsWeb ? "127.0.0.1:5000" : "10.0.2.2:5000",
+      child: App(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:varenya_professionals/main.dart';
+import 'package:varenya_professionals/main.dev.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Root());
+    await tester.pumpWidget(DevRoot());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

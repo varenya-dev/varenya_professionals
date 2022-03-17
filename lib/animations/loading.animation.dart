13211def05e002a4a98907637dad83e513c5d2ba
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-17 09:52:45 +0530

    Animation text centering

--- lib/animations/loading.animation.dart
+++ lib/animations/loading.animation.dart
@@ -29,6 +29,7 @@ class Loading extends StatelessWidget {
             style: TextStyle(
               fontSize: MediaQuery.of(context).size.height * 0.03,
             ),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
